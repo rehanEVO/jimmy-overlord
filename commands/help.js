ea39bc3645c81260ec3b7a6447e1231c6ac09af4
MightBeASlut <38793289+MightBeASlut@users.noreply.github.com>
2018-04-27 14:21:19 -0700

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  let sicon = message.guild.iconURL;
+  let helpembed = new Discord.RichEmbed()
+  .setDescription("**Help Commands**")
+  .setColor("#930000")
+  .setThumbnail(sicon)
+  .addField("COMMAND", "MEMBERS", "STAFF", "Description", true)
+  .addField("wtb", "✅", "✅", "Wanting to buy something use -wtb _info here_", true)
+  .setFooter("Made by Jimmy ✦ Cracking Hacking");
+
+
+  return message.channel.send(helpembed);
+}
+
+module.exports.help = {
+  name:"help"
+}
