dd3c5b285376da8150d8000496fd90090c0abc19
MightBeASlut <38793289+MightBeASlut@users.noreply.github.com>
2018-04-27 14:07:40 -0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -8,8 +8,8 @@ module.exports.run = async (bot, message, args) => {
   let wtbEmbed = new Discord.RichEmbed()
   .setDescription("[WTB]")
   .setColor("#930000")
-  .addField("User", `${message.author}`)
-  .addField("INFO", info)
+  .addField("User", `[${message.author}]` true)
+  .addField("INFO", info true)
   .setFooter("Made by Jimmy ✦ Cracking Hacking");
 
   let wtbchannel = message.guild.channels.find(`name`, "wtb")
