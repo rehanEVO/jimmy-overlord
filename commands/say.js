390e94882032e3434f31dded0472d425cb3aab40
MightBeASlut <38793289+MightBeASlut@users.noreply.github.com>
2018-04-27 14:06:04 -0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   message.delete();
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
   let info = args.join(" ");
   
   let wtbEmbed = new Discord.RichEmbed()
