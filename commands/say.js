967b060fee78ebe4bb96f9ca01efbd1ba7c558ea
MightBeASlut <38793289+MightBeASlut@users.noreply.github.com>
2018-04-27 14:04:35 -0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -4,8 +4,20 @@ module.exports.run = async (bot, message, args) => {
 
   message.delete();
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
-  let botmessage = args.join(" ");
-  message.channel.send(botmessage);
+  let info = args.join(" ");
+  
+  let wtbEmbed = new Discord.RichEmbed()
+  .setDescription("[WTB]")
+  .setColor("#930000")
+  .addField("User", `${message.author}`)
+  .addField("INFO", info)
+  .setFooter("Made by Jimmy ✦ Cracking Hacking");
+
+  let wtbchannel = message.guild.channels.find(`name`, "wtb")
+  if(!wtbchannel) return message.channel.send("Couldn't find wtb channel");
+
+  message.delete().catch(O_o=>{});
+  wtbchannel.send(wtbEmbed);
 }
 
 module.exports.help = {
