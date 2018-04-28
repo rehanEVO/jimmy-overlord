9c9d4daf0550d38eca3164b8ca8c891132880dc5
MightBeASlut <38793289+MightBeASlut@users.noreply.github.com>
2018-04-28 07:43:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ fs.readdir("./commands/", (err, files) =>{
   jsfile.forEach((f, i) =>{
     let props = require(`./commands/${f}`);
     console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
 
   });
 
