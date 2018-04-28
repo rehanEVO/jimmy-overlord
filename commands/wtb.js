4601d264aac421ee322a0f9f73cc528a5a0d88a6
MightBeASlut <38793289+MightBeASlut@users.noreply.github.com>
2018-04-28 06:13:38 -0700

    Update wtb.js

--- commands/wtb.js
+++ commands/wtb.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
 
   let wtbchannel = message.guild.channels.find(`name`, "wtb") 
   if(!wtbchannel) return message.channel.send({embed: {
-  description: ":x:  Couldn't find a wtb channel",
+  description: ":x:  Couldn't find a wtb channel.",
   color: 0x76070b
 }}).then(msg => {
    msg.delete(10000)
@@ -22,7 +22,7 @@ module.exports.run = async (bot, message, args) => {
 
   wtbchannel.send(wtbEmbed);
   message.channel.send({embed: {
-  description: ":white_check_mark:  Your request has been added to #wtb",
+  description: ":white_check_mark:  Your request has been added to #wtb.",
   color: 0x32e01f
 }}).then(msg => {
    msg.delete(10000)
