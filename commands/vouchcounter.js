3cd061289655c2899227453ec5ea160ba1d4e85e
MightBeASlut <38793289+MightBeASlut@users.noreply.github.com>
2018-04-29 13:49:30 -0700

    Create vouchcounter.js

---
+++ commands/vouchcounter.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+let vouches = JSON.parse(fs.readFileSync("./vouches.json", "utf8"));
+
+module.exports.run = async (bot, message, args) => {
+
+  let vUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+  if(!vUser) return message.reply("Can't find user.");
+  let vouchcounter = vouches[vUser.id].vouches;
+
+  message.reply(`<@${vUser.id}> has ${vouchcounter} vouches.`);
+
+}
+
+module.exports.help = {
+  name: "vouches"
+}
