f24cd02a1d8b7a330d566a6e64a04b99d88901a6
MightBeASlut <38793289+MightBeASlut@users.noreply.github.com>
2018-04-29 14:28:59 -0700

    Update vouchcounter.js

--- commands/vouchcounter.js
+++ commands/vouchcounter.js
@@ -7,6 +7,9 @@ module.exports.run = async (bot, message, args) => {
 
   let vUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!vUser) return message.reply("Can't find user.");
+
+  vouches[vUser.id].vouches = 0;
+
   let vouchcounter = vouches[vUser.id].vouches;
 
   message.reply(`<@${vUser.id}> has ${vouchcounter} vouches.`);
